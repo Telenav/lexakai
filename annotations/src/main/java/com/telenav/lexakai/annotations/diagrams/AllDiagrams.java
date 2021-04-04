2c8f7ab82ac59661a6e45390de4910a29e08f97f
Jonathan Locke <jonathanl@telenav.com>
2021-04-04 08:00:58 -0700

    ♻️ Remove applications

--- annotations/src/main/java/com/telenav/lexakai/annotations/diagrams/AllDiagrams.java
+++
@@ -1,8 +0,0 @@
-package com.telenav.lexakai.annotations.diagrams;
-
-/**
- * @author jonathanl (shibo)
- */
-public interface AllDiagrams extends UmlDiagramIdentifier
-{
-}
--- annotations/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module lexakai.annotations
-{
-    exports com.telenav.lexakai.annotations;
-    exports com.telenav.lexakai.annotations.associations;
-    exports com.telenav.lexakai.annotations.diagrams;
-    exports com.telenav.lexakai.annotations.visibility;
-    exports com.telenav.lexakai.annotations.repeaters;
-}

8d1c13a28dd67aa11bede70c43508a9e5395db68
jonathan <jon@thanlocke.com>
2021-09-03 21:20:38 -0700

    👷 Add build feature branch workflow

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -21,7 +21,7 @@ CLONE="git clone --branch "$BRANCH" --quiet"
 # Build kivakit
 #
 
-echo "Cloning kivakit in $ROOT"
+echo "Cloning kivakit"
 cd "$ROOT"
 $CLONE https://github.com/Telenav/kivakit.git
 
@@ -37,7 +37,7 @@ $BUILD
 # Build kivakit-extensions
 #
 
-echo "Cloning kivakit-extensions in $ROOT"
+echo "Cloning kivakit-extensions"
 cd "$ROOT"
 $CLONE https://github.com/Telenav/kivakit-extensions.git
 
@@ -49,7 +49,7 @@ $BUILD
 # Build lexakai
 #
 
-echo "Cloning lexakai in $ROOT"
+echo "Cloning lexakai"
 cd "$ROOT"
 $CLONE https://github.com/Telenav/lexakai.git
 

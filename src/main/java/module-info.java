570a390eb57f7611cfd45def60d13f50a0a44aeb
jonathan <jon@thanlocke.com>
2021-10-13 16:00:28 -0700

    🚧 Fix microservice rest service regression  - Fix transitive keyword usage in module-info.java files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,22 +1,10 @@
 open module lexakai
 {
-    requires transitive lexakai.annotations;
-
-    requires transitive kivakit.application;
-    requires transitive kivakit.resource;
-    requires transitive kivakit.network.http;
+    requires kivakit.application;
+    requires kivakit.network.http;
 
     requires com.github.javaparser.core;
     requires com.github.javaparser.symbolsolver.core;
 
     exports com.telenav.lexakai;
-    exports com.telenav.lexakai.javadoc;
-    exports com.telenav.lexakai.associations;
-    exports com.telenav.lexakai.library;
-    exports com.telenav.lexakai.dependencies;
-    exports com.telenav.lexakai.builders;
-    exports com.telenav.lexakai.builders.grouper;
-    exports com.telenav.lexakai.indexes;
-    exports com.telenav.lexakai.members;
-    exports com.telenav.lexakai.types;
 }

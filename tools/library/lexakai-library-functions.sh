bc2759157e15983f12d40b4776fb978589853336
jonathan <jon@thanlocke.com>
2021-11-05 11:46:54 -0700

    👷 Prepare for 1.1.1 release
    ### added
    
       * [Add .proto output files to kivakit-microservice GRPC service](https://github.com/Telenav/kivakit/issues/89)
    
    ### fixed
    
      * [Division microservice example is broken #87](https://github.com/Telenav/kivakit/issues/87)
      * [Broken listener chain due to inheritance from mixin and base component #86](https://github.com/Telenav/kivakit/issues/86)
      * [Documentation links are broken in kivakit 1.1.0 #88](https://github.com/Telenav/kivakit/issues/88)

--- tools/library/lexakai-library-functions.sh
+++ tools/library/lexakai-library-functions.sh
@@ -66,7 +66,6 @@ clean_cache() {
 
         if [ -d "$cache" ]; then
             read -p "┋ Remove ALL cached files in $cache (y/n)? " -n 1 -r
-            echo "┋ "
             if [[ $REPLY =~ ^[Yy]$ ]]; then
                 rm -rf $cache
             fi
@@ -82,7 +81,7 @@ clean_maven_repository() {
         project_home=$1
         name=$(basename -- "$project_home")
 
-        if yes_no "Remove all $name artifacts from ~/.m2"; then
+        if yes_no "┋ Remove all $name artifacts from ~/.m2"; then
 
             rm -rf "~/.m2/repository/com/telenav/$name"
 
@@ -97,7 +96,7 @@ remove_maven_repository() {
 
         if [ -d "$HOME/.m2/repository" ]; then
 
-            if yes_no "Remove ALL artifacts in ~/.m2/repository"; then
+            if yes_no "┋ Remove ALL artifacts in ~/.m2/repository"; then
 
                 rm -rf ~/.m2/repository
 
@@ -114,7 +113,7 @@ clean_temporary_files() {
 
         project_home=$1
 
-        if yes_no "Remove temporary files (.DS_Store, .metadata, .classpath, .project, *.hprof, *~) from $project_home tree"; then
+        if yes_no "┋ Remove temporary files (.DS_Store, .metadata, .classpath, .project, *.hprof, *~) from $project_home tree"; then
 
             find $project_home \( -name \.DS_Store -o -name \.metadata -o -name \.classpath -o -name \.project -o -name \*\.hprof -o -name \*~ \) | xargs rm
 
